ea500e163b664c3d96a0163a28f8eb703aed718b
arpitdeveloper <arpit.developer101@gmail.com>
2018-04-25 16:15:20 +0530

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  FirebaseJokes
+//
+//  Created by Apple on 10/04/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://joke-appcoda-demo.firebaseio.com"
